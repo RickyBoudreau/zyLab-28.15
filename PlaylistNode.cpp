640def3c13ed7baf3c95cb0fb61a56b6c4b8a39c
Ricky Boudreau <rboudreau@smu.edu>
2023-12-05 17:53:14 -0600

    Began PlaylistNode.cpp -- needs to be implemented
    Should be the last thing we have to do. Just need to implement the basic functions of this class definition file

--- PlaylistNode.cpp
+++ PlaylistNode.cpp
@@ -1,1 +1,31 @@
-//code here
+#include <iostream>
+#include <iomanip>
+#include "PlaylistNode.h" 
+using namespace std;
+
+//for copying reference easily
+//PlaylistNode::
+
+    PlaylistNode::PlaylistNode()
+    {
+        uniqueID = "";
+        songName = "";
+        artistName = "";
+        songLength = -1;
+        nextNodePtr = nullptr;
+    }
+
+    PlaylistNode::PlaylistNode(string uniqueID, string songName, string artistName, int songLength);
+
+
+    string PlaylistNode::GetID() const;
+    string PlaylistNode::GetSongName() const;
+    string PlaylistNode::GetArtistName() const;
+    int PlaylistNode::GetSongLength() const;
+    PlaylistNode* PlaylistNode::GetNext() const;
+
+
+    void PlaylistNode::InsertAfter(PlaylistNode* nodePtr);
+    void PlaylistNode::SetNext(PlaylistNode* nodePtr);
+
+    void PlaylistNode::PrintPlaylistNode() const;
