0adff1e5fca04f57d65922f1067860ea9aab0d01
daveybair <72879558+daveybair@users.noreply.github.com>
2023-12-06 01:33:02 -0600

    Update PlaylistNode.cpp

--- PlaylistNode.cpp
+++ PlaylistNode.cpp
@@ -8,24 +8,39 @@ using namespace std;
 
     PlaylistNode::PlaylistNode()
     {
-        uniqueID = "";
-        songName = "";
-        artistName = "";
-        songLength = -1;
+        uniqueID = "none";
+        songName = "none";
+        artistName = "none";
+        songLength = 0;
         nextNodePtr = nullptr;
     }
 
-    PlaylistNode::PlaylistNode(string uniqueID, string songName, string artistName, int songLength);
+    PlaylistNode::PlaylistNode(string uniqueID, string songName, string artistName, int songLength) {
+        this->uniqueID = uniqueID;
+        this->songName = songName;
+        this->artistName = artistName;
+        this->songLength = songLength;
+        nextNodePtr = nullptr;
+    }
 
 
-    string PlaylistNode::GetID() const;
-    string PlaylistNode::GetSongName() const;
-    string PlaylistNode::GetArtistName() const;
-    int PlaylistNode::GetSongLength() const;
-    PlaylistNode* PlaylistNode::GetNext() const;
+    string PlaylistNode::GetID() const{return uniqueID;}
+    string PlaylistNode::GetSongName() const{return songName;}
+    string PlaylistNode::GetArtistName() const{return artistName;}
+    int PlaylistNode::GetSongLength() const{return songLength;}
+    PlaylistNode* PlaylistNode::GetNext() const{return nextNodePtr;}
 
 
-    void PlaylistNode::InsertAfter(PlaylistNode* nodePtr);
-    void PlaylistNode::SetNext(PlaylistNode* nodePtr);
+    void PlaylistNode::InsertAfter(PlaylistNode* nodePtr){
+        PlaylistNode* tmpNext = nextNodePtr;
+        nextNodePtr = nodePtr;
+        nodePtr->nextNodePtr = tmpNext;
+    }
+    void PlaylistNode::SetNext(PlaylistNode* nodePtr) {nextNodePtr = nodePtr;}
 
-    void PlaylistNode::PrintPlaylistNode() const;
+    void PlaylistNode::PrintPlaylistNode() const{
+        cout << "Unique ID: " << uniqueID << endl;
+        cout << "Song Name: " << songName << endl;
+        cout << "Artist Name: " << artistName << endl;
+        cout << "Song Length (in seconds): " << songLength << endl;
+    }
